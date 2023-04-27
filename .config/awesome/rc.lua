f402ae695c54f3cfd8556f58715914bdc70651fb
Jorge Bodega <jorge.bodega.f@gmail.com>
2023-04-27 13:05:00 +0200

    more awesome config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -272,65 +272,7 @@ require("keybindings.screen")
 require("keybindings.tag")
 
 -- Client related keybindings
-awful.keyboard.append_global_keybindings({
-	awful.key({ super }, "u", awful.client.urgent.jumpto, { description = "jump to urgent client", group = "client" }),
-
-	-- Focus the previous/next clients in the current tag
-	awful.key({ super }, "j", function()
-		awful.client.focus.byidx(1)
-	end, { description = "focus the next client by index", group = "client" }),
-	awful.key({ super }, "k", function()
-		awful.client.focus.byidx(-1)
-	end, { description = "focus the previous client by index", group = "client" }),
-
-	-- Swap with the previous/next clients in the current tag
-	awful.key({ super, "Shift" }, "j", function()
-		awful.client.swap.byidx(1)
-	end, { description = "swap with next client by index", group = "client" }),
-	awful.key({ super, "Shift" }, "k", function()
-		awful.client.swap.byidx(-1)
-	end, { description = "swap with previous client by index", group = "client" }),
-})
-
-awful.keyboard.append_client_keybindings({
-	-- awful.key({ super }, "f", function(c)
-	-- 	c.fullscreen = not c.fullscreen
-	-- 	c:raise()
-	-- end, { description = "toggle fullscreen", group = "client" }),
-
-	-- Close client
-	awful.key({ super }, "q", function(client)
-		client:kill()
-	end, { description = "close client", group = "client" }),
-
-	awful.key({ "Control" }, "space", function(c)
-		awful.client.floating.toggle(c)
-		c.width = 1000
-		c.height = 550
-		awful.placement.centered(c)
-	end, { description = "toggle floating", group = "client" }),
-	awful.key({ super, "Control" }, "Return", function(c)
-		c:swap(awful.client.getmaster())
-	end, { description = "move to master", group = "client" }),
-	awful.key({ super }, "o", function(c)
-		c:move_to_screen()
-	end, { description = "move to screen", group = "client" }),
-	-- awful.key({ super }, "t", function(c)
-	-- 	c.ontop = not c.ontop
-	-- end, { description = "toggle keep on top", group = "client" }),
-	-- awful.key({ super }, "m", function(c)
-	-- 	c.maximized = not c.maximized
-	-- 	c:raise()
-	-- end, { description = "(un)maximize", group = "client" }),
-	-- awful.key({ super, "Control" }, "m", function(c)
-	-- 	c.maximized_vertical = not c.maximized_vertical
-	-- 	c:raise()
-	-- end, { description = "(un)maximize vertically", group = "client" }),
-	-- awful.key({ super, "Shift" }, "m", function(c)
-	-- 	c.maximized_horizontal = not c.maximized_horizontal
-	-- 	c:raise()
-	-- end, { description = "(un)maximize horizontally", group = "client" }),
-})
+require("keybindings.client")
 
 -- Launcher related keybindings
 awful.keyboard.append_global_keybindings({
@@ -531,3 +473,10 @@ gears.timer({
 		collectgarbage()
 	end,
 })
+
+-- Autostart
+
+awful.spawn.with_shell("sh ~/.fehbg")
+awful.spawn.with_shell("sh ~/.config/awesome/autorun.sh")
+-- awful.spawn.with_shell("pkill http-server")
+-- awful.spawn.with_shell("http-server ~/.config/chevron/dist")
