ef9378d0bf1da40c1fc22c474f31dd1697d30156
Jorge Bodega <jorge.bodega.f@gmail.com>
2023-12-24 19:53:45 +0100

    wip: which key

--- new-nvim/lua/plugins/neo-tree.lua
+++ new-nvim/lua/plugins/neo-tree.lua
@@ -1,23 +1,16 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-      enable_diagnostics = true,
-      enable_git_status = true,
-      enable_modified_markers = true,
-      popup_border_style = "single",
-    })
-  end,
+  cmd = { "Neotree" },
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
   },
-  keys = {
-    { "<leader>ff", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
-    { "<leader>fl", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
-    { "<leader>fr", ":Neotree toggle right<CR>", silent = true, desc = "Right File Explorer" },
+  opts = {
+    close_if_last_window = true,
+    enable_diagnostics = true,
+    enable_git_status = true,
+    enable_modified_markers = true,
+    popup_border_style = "single",
   },
-  lazy = true,
 }
